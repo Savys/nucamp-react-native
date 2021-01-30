f084557fe5ca70485c46b270e5f840be54ac9296
Savitha Srinivasan <savysree2@gmail.com>
2021-01-30 11:55:22 -0800

    Assignment1:Task1

---
+++ components/AboutComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {ScrollView} from 'react-native';
+
+export default class About extends Component{
+    static navigationOptions ={
+        title:'ABout Us'
+    }
+    render(){
+        return(
+            <ScrollView></ScrollView>
+        )
+
+    }
+
+}
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
