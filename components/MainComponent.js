dbb15ef1ff0ca8b44d968dc0c5b6fab38f62af6b
Savitha Srinivasan <savysree2@gmail.com>
2021-01-29 22:21:46 -0800

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react';
-import Directory from './DirectoryComponent';	
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
+import { View, Platform } from 'react-native';
+import { DISHES } from '../shared/dishes';
+import { createStackNavigator } from 'react-navigation';
 
-import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';	import { CAMPSITES } from '../shared/campsites';
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  DishDetail: { screen: DishDetail }
+}, {
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"            
+      }
+  }
+}
+);
 
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null
+    };
+  }
 
-class Main extends Component {	class Main extends Component {
-    constructor(props) {	    constructor(props) {
-        super(props);	        super(props);
-        this.state = {	        this.state = {
-            campsites: CAMPSITES	            campsites: CAMPSITES,
-            selectedCampsite: null
-        };	        };
-    }	    }
+  onDishSelect(dishId) {
+    this.setState({selectedDish: dishId})
+}
 
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId})
-    }
-
-    render() {	    render() {
-        return <Directory campsites={this.state.campsites} />;	        return (
-            <View style={{flex: 1}}>
-                <Directory 
-                    campsites={this.state.campsites} 
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id == this.state.selectedCampsite)[0]}
-                />
-            </View>
-        );
-    }	    }
-}	}
-
-
+  render() {
+ 
+    return (
+      <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+        <MenuNavigator />
+      </View>
+    );
+  }
+}
+  
+export default Main;
